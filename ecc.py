2e0e534e78637b0478235284cab86f79e4a46052
shitcoding <shitcoder@pm.me>
2022-07-08 20:15:17 +0300

    implement addition and substraction for finite field element

--- ecc.py
+++ ecc.py
@@ -15,3 +15,19 @@ class FieldElement:
         if other is None:
             return False
         return self.num == other.num and self.prime == other.prime
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def __add__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot add two numbers in different Fields')
+        num = (self.num + other.num) % self.prime
+        return self.__class__(num, self.prime)
+
+    def __sub__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot substract two numbers in different Fields')
+        num = (self.num - other.num) % self.prime
+        return self.__class__(num, self.prime)
+
