66c669b9558dd15c2cc508df6c281dd4bc4173ac
shitcoding <shitcoder@pm.me>
2022-07-08 22:40:59 +0300

    implement exponent and division for finite field element

--- ecc.py
+++ ecc.py
@@ -8,16 +8,20 @@ class FieldElement:
         self.num = num
         self.prime = prime
 
+
     def __repr__(self):
         return f'FieldElement_{self.prime}({self.num})'
+
 
     def __eq__(self, other):
         if other is None:
             return False
         return self.num == other.num and self.prime == other.prime
 
+
     def __ne__(self, other):
         return not (self == other)
+
 
     def __add__(self, other):
         if self.prime != other.prime:
@@ -25,15 +29,30 @@ class FieldElement:
         num = (self.num + other.num) % self.prime
         return self.__class__(num, self.prime)
 
+
     def __sub__(self, other):
         if self.prime != other.prime:
             raise TypeError('Cannot substract two numbers in different Fields')
         num = (self.num - other.num) % self.prime
         return self.__class__(num, self.prime)
 
+
     def __mul__(self, other):
         if self.prime != other.prime:
             raise TypeError('Cannot multiply two numbers in different Fields')
         num = (self.num * other.num) % self.prime
+        return self.__class__(num, self.prime)
+
+
+    def __pow__(self, exponent):
+        n = exponent % (self.prime - 1)
+        num = pow(self.num, n, self.prime)
+        return self.__class__(num, self.prime)
+
+
+    def __truediv__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot divide two numbers in different Fields')
+        num = (self.num * pow(other.num, self.prime-2, self.prime)) % self.prime
         return self.__class__(num, self.prime)
 
