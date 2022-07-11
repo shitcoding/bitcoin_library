6970bd7e2e9590a426614919f717ec680ee5dc6a
shitcoding <shitcoder@pm.me>
2022-07-12 00:04:29 +0300

    implement private key class with message signing functionality

--- py_bitcoin/ecc.py
+++ py_bitcoin/ecc.py
@@ -1,3 +1,7 @@
+import hashlib
+import hmac
+
+
 class FieldElement:
     """Finite field element."""
 
@@ -172,7 +176,6 @@ class S256Point(Point):
         else:
             return 'S256Point({}, {})'.format(self.x, self.y)
 
-
     def __rmul__(self, coefficient):
         # We can mod by n because nG = 0
         coef = coefficient % N
@@ -209,3 +212,62 @@ class Signature:
 
     def __repr__(self):
         return 'Signature({:x},{:x})'.format(self.r, self.s)
+
+
+class PrivateKey:
+    """
+    Private key used for signing messages and transactions.
+    """
+    def __init__(self, secret):
+        """
+        secret: secret used for private key generation
+        point: public key
+        """
+        self.secret = secret
+        self.point = secret * G
+
+    def hex(self):
+        """Return hex representation of private key."""
+        return '{:x}'.format(self.secret).zfill(64)
+
+    def deterministic_k(self, z):
+        """
+        Generate unique deterministic random number
+        according to RFC 6979 specification.
+        """
+        k = b'\x00' * 32
+        v = b'\x01' * 32
+        if z > N:
+            z -= N
+        z_bytes = z.to_bytes(32, 'big')
+        secret_bytes = self.secret.to_bytes(32, 'big')
+        s256 = hashlib.sha256
+        k = hmac.new(k, v + b'\x00' + secret_bytes + z_bytes, s256).digest()
+        v = hmac.new(k, v, s256).digest()
+        k = hmac.new(k, v + b'\x01' + secret_bytes + z_bytes, s256).digest()
+        v = hmac.new(k, v, s256).digest()
+        while True:
+            v = hmac.new(k, v, s256).digest()
+            candidate = int.from_bytes(v, 'big')
+            if candidate >= 1 and candidate < N:
+                return candidate
+            k = hmac.new(k, v + b'\x00', s256).digest()
+            v = hmac.new(k, v, s256).digest()
+
+    def sign(self, z):
+        """
+        Sign a message or transaction with a private key.
+
+        args:
+            z: signature hash
+
+        returns:
+            signature
+        """
+        k = self.deterministic_k(z)
+        r = (k*G).x.num
+        k_inv = pow(k, N - 2, N)
+        s = (z + r * self.secret) * k_inv % N
+        if s > N / 2:
+            s = N - s
+        return Signature(r, s)
