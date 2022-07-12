b502a5b578f786470e0f34e0ba92f6bfa8077c87
shitcoding <shitcoder@pm.me>
2022-07-13 01:13:13 +0300

    implement signature DER format serialization

--- py_bitcoin/ecc.py
+++ py_bitcoin/ecc.py
@@ -253,6 +253,24 @@ class Signature:
     def __repr__(self):
         return 'Signature({:x},{:x})'.format(self.r, self.s)
 
+    def der(self):
+        """Return serialized signature in DER format."""
+        rbin = self.r.to_bytes(32, byteorder='big')
+        # remove all null bytes at the beginning
+        rbin = rbin.lstrip(b'\x00')
+        # if rbin has a high bit, add a \x00
+        if rbin[0] & 0x80:
+            rbin = b'\x00' + rbin
+        result = bytes([2, len(rbin)]) + rbin
+        sbin = self.s.to_bytes(32, byteorder='big')
+        # remove all null bytes at the beginning
+        sbin = sbin.lstrip(b'\x00')
+        # if sbin has a high bit, add a \x00
+        if sbin[0] & 0x80:
+            sbin = b'\x00' + sbin
+        result += bytes([2, len(sbin)]) + sbin
+        return bytes([0x30, len(result)]) + result
+
 
 class PrivateKey:
     """
