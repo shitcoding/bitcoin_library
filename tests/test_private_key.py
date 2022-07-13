7fb9f103b2d5cdb8b293c1421cbcf6b5c80cb756
shitcoding <shitcoder@pm.me>
2022-07-13 17:31:36 +0300

    tests refactoring

--- tests/test_private_key.py
+++ tests/test_private_key.py
@@ -35,29 +35,31 @@ def test_signature_verification():
     assert pk.point.verify(z, sig)
 
 
+SEC_TEST_CASES = (
+    (
+        999**3,
+        '049d5ca49670cbe4c3bfa84c96a8c87df086c6ea6a24ba6b809c9de234496808d56fa15cc7f3d38cda98dee2419f415b7513dde1301f8643cd9245aea7f3f911f9',
+        '039d5ca49670cbe4c3bfa84c96a8c87df086c6ea6a24ba6b809c9de234496808d5',
+    ),
+    (
+        123,
+        '04a598a8030da6d86c6bc7f2f5144ea549d28211ea58faa70ebf4c1e665c1fe9b5204b5d6f84822c307e4b4a7140737aec23fc63b65b35f86a10026dbd2d864e6b',
+        '03a598a8030da6d86c6bc7f2f5144ea549d28211ea58faa70ebf4c1e665c1fe9b5',
+    ),
+    (
+        42424242,
+        '04aee2e7d843f7430097859e2bc603abcc3274ff8169c1a469fee0f20614066f8e21ec53f40efac47ac1c5211b2123527e0e9b57ede790c4da1e72c91fb7da54a3',
+        '03aee2e7d843f7430097859e2bc603abcc3274ff8169c1a469fee0f20614066f8e',
+    ),
+)
+
+
 def test_s256point_serialization_to_sec():
     """
     Testing S256Point serialization to uncompressed and
     compressed binary SEC format.
     """
-    test_cases = (
-        (
-            999**3,
-            '049d5ca49670cbe4c3bfa84c96a8c87df086c6ea6a24ba6b809c9de234496808d56fa15cc7f3d38cda98dee2419f415b7513dde1301f8643cd9245aea7f3f911f9',
-            '039d5ca49670cbe4c3bfa84c96a8c87df086c6ea6a24ba6b809c9de234496808d5',
-        ),
-        (
-            123,
-            '04a598a8030da6d86c6bc7f2f5144ea549d28211ea58faa70ebf4c1e665c1fe9b5204b5d6f84822c307e4b4a7140737aec23fc63b65b35f86a10026dbd2d864e6b',
-            '03a598a8030da6d86c6bc7f2f5144ea549d28211ea58faa70ebf4c1e665c1fe9b5',
-        ),
-        (
-            42424242,
-            '04aee2e7d843f7430097859e2bc603abcc3274ff8169c1a469fee0f20614066f8e21ec53f40efac47ac1c5211b2123527e0e9b57ede790c4da1e72c91fb7da54a3',
-            '03aee2e7d843f7430097859e2bc603abcc3274ff8169c1a469fee0f20614066f8e',
-        ),
-    )
-    for (coef, uncompressed_sec, compressed_sec) in test_cases:
+    for (coef, uncompressed_sec, compressed_sec) in SEC_TEST_CASES:
         point = coef * G
         assert point.sec(compressed=False) == bytes.fromhex(uncompressed_sec)
         assert point.sec(compressed=True) == bytes.fromhex(compressed_sec)
