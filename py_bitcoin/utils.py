5bb2806e6d4c2433919f451d9bdfdbd889b93d64
shitcoding <shitcoder@pm.me>
2022-07-14 12:14:04 +0300

    implement functions for reading and encoding variable integers

--- py_bitcoin/utils.py
+++ py_bitcoin/utils.py
@@ -56,3 +56,34 @@ def little_endian_to_int(binary):
 def int_to_little_endian(num, length):
     """Return little-endian byte sequence from given integer."""
     return num.to_bytes(length, 'little')
+
+
+def read_varint(stream):
+    """Read variable integer from a stream."""
+    i = stream.read(1)[0]
+    if i == 0xfd:
+        # 0xfd means the next 2 bytes are the number
+        return little_endian_to_int(stream.read(2))
+    elif i == 0xfe:
+        # 0xfe means the next 4 bytes are the number
+        return little_endian_to_int(stream.read(4))
+    elif i == 0xff:
+        # 0xff means the next 8 bytes are the number
+        return little_endian_to_int(stream.read(8))
+    else:
+        # anything else is just the integer
+        return i
+
+
+def encode_varint(i):
+    """Encode an integer as a variable integer."""
+    if i < 0xfd:
+        return bytes([i])
+    elif i < 0x10000:
+        return b'\xfd' + int_to_little_endian(i, 2)
+    elif i < 0x100000000:
+        return b'\xfe' + int_to_little_endian(i, 4)
+    elif i < 0x10000000000000000:
+        return b'\xff' + int_to_little_endian(i, 8)
+    else:
+        raise ValueError(f'integer is too large: {i}')
